b3452c91e9c0a5cc7379165e0aaab5548bcc55c6
Bashir Sule <sulebashir001@gmail.com>
2026-03-05 16:45:26 +0100

    Add Hardhat configuration for Solidity and networks

---
+++ hardhat.config.cjs
@@ -0,0 +1,17 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "https://rpc.sepolia.org",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 11155111
+    }
+  }
+};
